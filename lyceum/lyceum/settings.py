09cddef47bdad7e0c93783a87346b072a0b4b92a
Damir <damirkdz@gmail.com>
2026-02-02 18:01:15 +0500

    секретни ключ2

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-fake-dope-lol-insecure-change-in-production"
+SECRET_KEY = "secret-key-here"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
