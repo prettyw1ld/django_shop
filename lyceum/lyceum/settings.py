9881a79f50b9e086a4c0fc3bf004cf5bb209f819
Damir <damirkdz@gmail.com>
2026-02-06 00:16:27 +0500

    дебаг опят

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fake-key")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "t", "f", "0")
+DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "t")
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
