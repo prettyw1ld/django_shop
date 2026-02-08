7ab6903303e790b02371a5bb91c189ca17f47aab
Damir <damirkdz@gmail.com>
2026-02-08 23:21:22 +0500

    тест и ссылки

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+import catalog.views
+import homepage.views
 from django import conf, urls
 from django.contrib import admin
 from django.urls import include, path
-
-import catalog.views
-
-import homepage.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
