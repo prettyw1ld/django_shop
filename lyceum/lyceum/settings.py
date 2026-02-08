995946916bafcb58d5c766256f7358e1d5078984
Damir <damirkdz@gmail.com>
2026-02-08 21:26:09 +0500

    Исправление: порядок в requ.../test.txt и settings.py

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fake-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "t", "yes", "y")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = [
+    os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(","),
+]
 
 # Application definition
 
