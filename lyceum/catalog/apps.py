b520221d1673ae84a115e79a0b6014e0dd820d1f
Damir <damirkhodzhiev@yandex.ru>
2026-02-20 00:28:25 +0500

    refactor: renaming... again...

--- lyceum/catalog/apps.py
+++ lyceum/catalog/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class CatalogConfig(AppConfig):
     name = "catalog"
-    verbose_name = "каталог"
+    verbose_name = "Каталог"
     default_auto_field = "django.db.models.BigAutoField"
--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -5,6 +5,7 @@ class PublishedBaseModel(django.db.models.Model):
     is_published = django.db.models.BooleanField(
         default=True,
         verbose_name="опубликовано",
+        help_text="статус публикации",
     )
 
     class Meta:
