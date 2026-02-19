77d1dfcda93d52cf2ff57f15bcbaf9fe84690228
Damir <damirkhodzhiev@yandex.ru>
2026-02-19 23:18:26 +0500

    refactor: black formating

--- lyceum/catalog/validators.py
+++ lyceum/catalog/validators.py
@@ -6,7 +6,7 @@ import django.core.validators
 def validate_brilliant(value):
     match_1 = re.search(r"\bпревосходно\b", value.lower())
     match_2 = re.search(r"\bроскошно\b", value.lower())
-    if (not match_1 and not match_2):
+    if not match_1 and not match_2:
         raise django.core.validators.ValidationError(
             "Обязательно используйте одно из этих "
             + "слов: превосходно, роскошно",
