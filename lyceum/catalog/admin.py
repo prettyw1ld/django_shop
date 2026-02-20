7580c05d270a0f249a10ff9823188a8f1ad81953
Damir <damirkhodzhiev@yandex.ru>
2026-02-20 13:29:21 +0500

    refactor: constants deleting

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -2,61 +2,50 @@ import django.contrib.admin
 
 import catalog.models
 
-category_name = catalog.models.Category.name.field.name
-category_published = catalog.models.Category.is_published.field.name
-tag_name = catalog.models.Tag.name.field.name
-item_name = catalog.models.Item.name.field.name
-item_published = catalog.models.Item.is_published.field.name
-item_tags = catalog.models.Item.tags.field.name
-
 
 @django.contrib.admin.register(catalog.models.Category)
 class CategoryAdmin(django.contrib.admin.ModelAdmin):
     list_display = (
-        category_name,
-        category_published,
+        catalog.models.Category.name.field.name,
+        catalog.models.Category.is_published.field.name,
     )
 
     @django.contrib.admin.display(description="Название", ordering="name")
     def name(self, obj):
-        return getattr(obj, category_name)
+        return getattr(obj, catalog.models.Category.name.field.name)
 
     @django.contrib.admin.display(
-        description="Опубликовано",
-        boolean=True,
-        ordering="is_published",
+        description="Опубликовано", boolean=True, ordering="is_published"
     )
     def is_published(self, obj):
-        return getattr(obj, category_published)
+        return getattr(obj, catalog.models.Category.is_published.field.name)
 
 
 @django.contrib.admin.register(catalog.models.Tag)
 class TagAdmin(django.contrib.admin.ModelAdmin):
-    list_display = (tag_name,)
+    list_display = (catalog.models.Tag.name.field.name,)
 
     @django.contrib.admin.display(description="Название", ordering="name")
     def name(self, obj):
-        return getattr(obj, tag_name)
+        return getattr(obj, catalog.models.Tag.name.field.name)
 
 
 @django.contrib.admin.register(catalog.models.Item)
 class ItemAdmin(django.contrib.admin.ModelAdmin):
     list_display = (
-        item_name,
-        item_published,
+        catalog.models.Item.name.field.name,
+        catalog.models.Item.is_published.field.name,
     )
-    list_editable = (item_published,)
-    list_display_links = (item_name,)
-    filter_horizontal = (item_tags,)
+    list_editable = (catalog.models.Item.is_published.field.name,)
+    list_display_links = (catalog.models.Item.name.field.name,)
+    filter_horizontal = (catalog.models.Item.tags.field.name,)
 
     @django.contrib.admin.display(description="Название", ordering="name")
     def name(self, obj):
-        return getattr(obj, item_tags)
+        return getattr(obj, catalog.models.Item.name.field.name)
 
     @django.contrib.admin.display(
-        description="Опубликовано",
-        boolean=True,
-        ordering="is_published",
+        description="Опубликовано", boolean=True, ordering="is_published"
     )
     def is_published(self, obj):
-        return getattr(obj, item_published)
+        return getattr(obj, catalog.models.Item.is_published.field.name)
