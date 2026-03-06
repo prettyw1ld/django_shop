fd74584a78973dbbf05e0d5a7036a82eeef1569d
Damir <damirkhodzhiev@yandex.ru>
2026-03-07 00:51:36 +0500

    feat: test

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,40 +1,10 @@
 __all__ = []
 
-from http import HTTPStatus
-
 from django.core.exceptions import ValidationError
-from django.test import Client, TestCase
-from django.urls import reverse
+from django.test import TestCase
 import parameterized
 
 from catalog.models import Category, Item, Tag
-
-
-class StaticURLTests(TestCase):
-    def test_catalog_endpoint(self):
-        response = self.client.get(reverse("catalog:item-list"))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    @parameterized.parameterized.expand(
-        [
-            ("1", HTTPStatus.OK),
-            ("100", HTTPStatus.OK),
-            ("0", HTTPStatus.OK),
-            ("-0", HTTPStatus.NOT_FOUND),
-            ("-100", HTTPStatus.NOT_FOUND),
-            ("0.5", HTTPStatus.NOT_FOUND),
-            ("abc", HTTPStatus.NOT_FOUND),
-            ("0abc", HTTPStatus.NOT_FOUND),
-            ("abc0", HTTPStatus.NOT_FOUND),
-            ("$#@", HTTPStatus.NOT_FOUND),
-            ("1e5", HTTPStatus.NOT_FOUND),
-            ("1_1", HTTPStatus.NOT_FOUND),
-            ("१२३", HTTPStatus.NOT_FOUND),
-        ],
-    )
-    def test_catalog_item_endpoint(self, url, expected_status):
-        response = Client().get(f"/catalog/{url}/")
-        self.assertEqual(response.status_code, expected_status)
 
 
 class TestModel(TestCase):
